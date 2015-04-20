f6640f8e55c1840b12dc8e12e11b9b32cb248b01
Tomasz Lichon <lichon@student.agh.edu.pl>
2015-04-20 19:13:01 +0300

    #7 Add Redis template

--- src/main/java/pl/edu/agh/student/config/RedisConfig.java
+++ src/main/java/pl/edu/agh/student/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package pl.edu.agh.student.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -13,4 +16,11 @@ public class RedisConfig {
 
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() throws Exception {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
 }
