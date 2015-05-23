1b6f561a98cf68c49aba789018f3e2fef4bd175b
sylgas <sylwia.m.gasior@gmail.com>
2015-05-24 00:45:13 +0200

    [CAL-49] Show only owner events

--- src/main/java/pl/edu/agh/student/service/EventService.java
+++ src/main/java/pl/edu/agh/student/service/EventService.java
@@ -27,6 +27,6 @@ public class EventService {
 
     public List<EventDto> getAllByCurrentUser(HttpServletRequest request) {
         User user = userService.getUserByHttpServletRequest(request);
-        return mapper.toDto(eventRepository.findAll());
+        return mapper.toDto(eventRepository.findByBaseDataOwner(user.getId()));
     }
 }
